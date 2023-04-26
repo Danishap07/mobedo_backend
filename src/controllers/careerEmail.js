ca732ab068b28748fcd7f1c749db2b6bceaab41d
Danish Patel <81384326+Danishap07@users.noreply.github.com>
2023-04-27 01:49:31 +0530

    Update careerEmail.js

--- src/controllers/careerEmail.js
+++ src/controllers/careerEmail.js
@@ -14,15 +14,26 @@ async function sendEmail(req, res) {
 
     const newpath = __dirname + "/files/";
     const files = req.files;
-    const attchmt = req.files.file;
+    if(files != undefined) {
+
+        var attchmt = req.files.file;
+        var filename = files.file.name;
+
+    }
+    else {
+        // console.log("jsdhb")
+        var attchmt = ""
+        var filename = ""
+        // emailWithAttachment()
+    }
     console.log("first", req.body);
-    const filename = files.file.name;
     const passData = req.body
-    // console.log("1", req)
+    console.log("1", attchmt)
+    console.log("files", files)
 
     
 
-    const email = async () => {
+    const emailWithAttachment = async () => {
         // if(passData) {
             // console.log("resod")
             const transporter = await nodemailer.createTransport({
@@ -36,9 +47,11 @@ async function sendEmail(req, res) {
                 }
             })
 
+            if (files != undefined) {
+
             let info = await transporter.sendMail({
                 from: '"Mobedo Email" <mobedowebsite@gmail.com>',
-                to: "careers@mobedoconsulting.com, info@mobedoconsulting.com",
+                to: "danishpatel915@gmail.com",
                 subject: "Mobedo User Details",
                 // text: "hello this is my first email from node.js.",
                 html: `<div style="background:#ecf2fb">
@@ -60,7 +73,8 @@ async function sendEmail(req, res) {
                     <p style="margin: 20px 0px 20px;  line-height: 1.5; font-size: 14px; color: #000;font-weight: 700;">
                         Mobile No: <span
                             style="margin: 20px 0px 20px;  line-height: 1.5; font-size: 14px; color: #000;font-weight: 500;">${passData.mobile_no}</span> </p>
-                    
+                     <p style="margin: 20px 0px 20px;  line-height: 1.5; font-size: 14px; color: #000;font-weight: 700;">
+                       
                 </div>
                 <div style="text-align:center">
                     <p style="font-size:11px;font-weight:600;color:#000">© Copyright 2023, All Rights Reserved | Mobedo
@@ -92,26 +106,88 @@ async function sendEmail(req, res) {
                 }
             }
             )
-        // }
+        }
+    else {
+             let info = await transporter.sendMail({
+                from: '"Mobedo Email" <mobedowebsite@gmail.com>',
+                to: "danishpatel915@gmail.com",
+                subject: "Mobedo User Details",
+                // text: "hello this is my first email from node.js.",
+                html: `<div style="background:#ecf2fb">
+        <div style="background:#ecf2fb;padding:30px 5px">
+            <div style="max-width:600px;min-width:295px;margin:0 auto">
+                <div
+                    style="background:linear-gradient(130.49deg,#006caa 10.49%,#e877fa 111.09%);padding:45px 30px;border-top-left-radius:10px;border-top-right-radius:10px">
+                    <div style="text-align:center;color:#fff;font-size:18px;font-weight:500">
+                        Form Submitted details are below
+                    </div>
+                </div>
+                <div style="background:#fff;padding:20px;border-bottom-left-radius:10px;border-bottom-right-radius:10px">
+                    <p style="margin: 20px 0px 20px;  line-height: 1.5; font-size: 14px; color: #000;font-weight: 700;">
+                        Name: <span
+                            style="margin: 20px 0px 20px;  line-height: 1.5; font-size: 14px; color: #000;font-weight: 500;">${passData.user_name}</span> </p>
+                    <p style="margin: 20px 0px 20px;  line-height: 1.5; font-size: 14px; color: #000;font-weight: 700;">
+                        Email Id: <span
+                            style="margin: 20px 0px 20px;  line-height: 1.5; font-size: 14px; color: #000;font-weight: 500;">${passData.user_email}</span> </p>
+                    <p style="margin: 20px 0px 20px;  line-height: 1.5; font-size: 14px; color: #000;font-weight: 700;">
+                        Mobile No: <span
+                            style="margin: 20px 0px 20px;  line-height: 1.5; font-size: 14px; color: #000;font-weight: 500;">${passData.mobile_no}</span> </p>
+                     <p style="margin: 20px 0px 20px;  line-height: 1.5; font-size: 14px; color: #000;font-weight: 700;">
+                        comments: <span
+                            style="margin: 20px 0px 20px;  line-height: 1.5; font-size: 14px; color: #000;font-weight: 500;">${passData.comments}</span> </p>
+                    
+                </div>
+                <div style="text-align:center">
+                    <p style="font-size:11px;font-weight:600;color:#000">© Copyright 2023, All Rights Reserved | Mobedo
+                        Consulting</p>
+                </div>
+            </div>
     
-
-    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    }
-            // console.log(req.files.file);
-            // console.log(file);
-            // console.log(Object.fromEntries(file));
-            attchmt.mv(`${newpath}${filename}`, async (err, res) => {
+        </div>`
+            }, (err, info) => {
                 if (err) {
+                    res.json({
+                        status: false,
+                        message: 'Email sending failed!'
+                    })
+                    console.log("Email sending failed", err);
+                }
+                else {
+                    res.json({
+                        status: true,
+                        message: "Message sent successfully!"
+                    })
+                    console.log("message sent successfully.");
+                }
+            }
+            )
+        }
+  
+            if(files === undefined) {
+                console.log("hello")
+           return emailWithAttachment()
+            
+        } else {
+            attchmt.mv(`${newpath}${filename}`, async (err, res) => {
+                    if (err) {
                     console.log(err)
                     res.status(500).json({ message: "File upload failed", response: err });
                 }
                 else {
-                    email()
+                    emailWithAttachment()
                     // res.json({ status:200, message: "File Uploaded", response: res, cb:email() });
                 }
             });
+        }
+            // email()
+       
+           
+    }
         
-}
+            }
+        
+        
+
 
 // main()
 
